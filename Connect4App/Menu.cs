f32b0041d8a936de8bf387885b81ac71f74ce31b
TimHaa <my.tim@web.de>
2018-05-20 17:04:59 +0200

    Added menu

---
+++ Connect4App/Menu.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Connect4App
+{
+    class Menu
+    {
+        private List<Player> players = new List<Player>();//get, set
+        public Menu(){ }
+
+        public void StartGame()
+        {
+            Board b = new Board();
+            players = SetPlayers();
+            ReassignOrder(players);
+            while (!b.PlayerWon)
+            {
+                foreach (Player p in players){
+                    b.GetInput(p.GetRow);
+                    if (b.PlayerWon)
+                    {
+                        Console.WriteLine("Congratulations {0}, you won!", b.victorious);
+                    }
+                }
+                
+            }
+            
+        }
+        private List<Player> SetPlayers()
+        {
+            List<Player> plrs = new List<Player>();
+            bool chooseAnother = true;
+            while (chooseAnother)
+            {
+                Console.WriteLine("Choose the type of Player you want to add: ");
+                switch (Console.ReadLine())
+                {
+                    case "human":
+                        Console.Write("Enter the name: ");
+                        string name = Console.ReadLine();
+                        Console.Write("Choose a letter as Symbol: ");
+                        string symbol = Console.ReadLine();
+                        Player newGuy = new HumanPlayer(name, symbol);//name and symbol in constructor of human player or here?
+                        plrs.Add(newGuy);
+                        break;
+                    case "rndBot":
+                        Player newRnd = new RandomBot();
+                        plrs.Add(newGuy);
+                        break;
+                    case "Bot":
+                        Player newBot = new SmartBot();
+                        plrs.Add(newGuy);
+                        break;
+                    case "s":
+                        chooseAnother = false;
+                        break;
+                    default:
+                        Console.WriteLine("1 for human Player - 2 for random Bot - 3 for smart Bot  - s to start the Game");
+                        break;
+                }
+            }
+            return plrs;
+        }
+    }
+}
--- Connect4App/Program.cs
+++ Connect4App/Program.cs
@@ -6,7 +6,18 @@ namespace Connect4App
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            /*Menu m = new Menu();
+            m.start();
+            get input for board and choose opponent types p1, p2,...
+            choose symbol
+            Menu.decideReihenfolge(){shuffle playerlist)
+            while(!Board.won){
+                foreach player in playerlist:
+                    Board.getInput(player.getRow(7))
+                if(Board.won){print Board.victorious}
+           
+        }
+        */
         }
     }
 }
